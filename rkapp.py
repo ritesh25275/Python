67c1fdbaf3d087896691f2e66754b2a50d2c9a11
ritesh25275 <73135644+ritesh25275@users.noreply.github.com>
2024-09-27 19:18:38 -0400

    Add files via upload

---
+++ rkapp.py
@@ -0,0 +1,8 @@
+def welcome_project(project_name):
+    print(f"Welcome to the {project_name} project!")
+    print("We are excited to have you on board.")
+    print("Let's work together to achieve great things!")
+
+# Example usage
+new_project = input("Enter the name of your new project: ")
+welcome_project(new_project)
