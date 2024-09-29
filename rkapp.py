9b9742131aafeba6da4d15dcfece846ce76052d1
ritesh25275 <73135644+ritesh25275@users.noreply.github.com>
2024-09-28 21:42:10 -0400

    Update rkapp.py

--- rkapp.py
+++ rkapp.py
@@ -1,8 +1,1 @@
-def welcome_project(project_name):
-    print(f"Welcome to the {project_name} project!")
-    print("We are excited to have you on board.")
-    print("Let's work together to achieve great things!")
-
-# Example usage
-new_project = input("Enter the name of your new project: ")
-welcome_project(new_project)
+print("Hello World")
